cb5819caff1236a29b1fdad0eba1919a1866a678
vaidehi-cell <vaidesai171202@gmail.com>
2022-06-22 15:12:28 +0530

    Added setting for static files

--- commerce/settings.py
+++ commerce/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'auctions\static'),
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'auctions/media') 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
